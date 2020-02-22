082aed5bbbe27a8afe83cb866efaa1c539724d72
Aravind-Sundararajan <asundar4@vols.utk.edu>
2020-02-22 09:57:46 -0500

    changed the fontforge script

--- fontScript.py
+++ fontScript.py
@@ -1,10 +1,12 @@
 #!/var/www/html
+import os
 import fontforge
 font = fontforge.font() # create a new font 
 
+
 for x in range(89):
 	glyph = font.createMappedChar(63+x)
-	nomo = "output\File" + str(x).zfill(3)+ ".svg"
+	nomo = os.path.join(os.getcwd(),'output','File' + str(x).zfill(3)+ '.svg')
 	glyph.importOutlines(nomo)
 
 font.generate("foobar.ttf") # truetype
